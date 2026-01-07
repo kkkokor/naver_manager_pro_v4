b92ba234674a25d3b7c4e198de175b02937b190d
kkkokor <charismacjy87.gmail.com>
2026-01-07 14:04:19 +0900

    aws 업로드

--- tailwind.config.js
+++ tailwind.config.js
@@ -7,7 +7,16 @@ export default {
     "./**/*.{js,ts,jsx,tsx}",
   ],
   theme: {
-    extend: {},
+    extend: {
+      // ▼▼▼ 이 부분이 빠져 있어서 색깔이 안 나왔던 겁니다! ▼▼▼
+      colors: {
+        naver: {
+          green: '#03C75A',
+          dark: '#02b351',
+          light: '#e5f9ee'
+        }
+      }
+    },
   },
   plugins: [],
 }
